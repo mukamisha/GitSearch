b057b64d4a0725107063e841118cbf08c280d3bb
mukamisha <mukamishajacky97@gmail.com>
2019-09-01 16:40:23 +0200

    adding a service to my project

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RepoClass } from '../repo-class';
+import { UserserviceService } from 'src/app/userservice.service';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -15,12 +16,14 @@ export class UserComponent implements OnInit {
       login:string;
       public_repos:number;
       bio:string;
-      public_gists:number
+      public_gists:number;
+      followers:number;
+      following:number
     }
   
       this.http.get<ApiResponse>("https://api.github.com/users/daneden?access_token=f36e6bc54204b7ed6fd3469a90f88b635e7bb767 ").subscribe(data=>{
         // Succesful API request
-        this.giti = new RepoClass(data. login, data.public_repos,data.bio , data.public_gists)
+        this.giti = new RepoClass(data. login, data.public_repos,data.bio , data.public_gists ,data.followers ,data.following)
       })
   }
 }
---
+++ src/app/userservice.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {environment } from '../environments/environment';
+import { RepoClass } from './repo-class';
+@Injectable({
+  providedIn: 'root'
+})
+export class UserserviceService {
+  giti: RepoClass;
+  constructor(private http:HttpClient) { 
+this.giti= new RepoClass("",0,"",0,0,0);
+// this.repolist =[];
+}
+
+UserserviceService(userName){
+  interface ApiResponse{
+    login:string;
+    public_repos:number;
+    bio:string;
+    public_gists:number;
+    followers:number;
+    following:number
+  }
+  let promise = new Promise((resolve,reject)=>{
+    this.http.get<ApiResponse>('https://api.github.com/users/'+userName + "?access_token=" + environment.apiUrl).toPromise().then(response=>{
+      this.giti.githubUserName=response.login;
+      this.giti.repositories =response.public_repos;
+      this.giti.biography=response.bio;
+      this.giti.gits=response.public_gists;
+      this.giti.followers=response.followers;
+      this.giti.following=response.following;
+      resolve()
+    },
+  // console.log(this.response)
+  error=>{
+    this.giti.githubUserName="invalid"
+  reject(error)
+    })
+  })
+  return promise
+}
+}
+
+
+
+
+
+
